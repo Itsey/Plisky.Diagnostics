96abe85398f816da8b962afc93f1ae5b7055cd46
Darren Stone <dbjstone@outlook.com>
2019-01-23 19:57:08 +0000

    Add convenience method for adding handlers

--- LibSrc/BilgeCore/Bilge/Bilge.cs
+++ LibSrc/BilgeCore/Bilge/Bilge.cs
@@ -102,6 +102,10 @@ namespace Plisky.Diagnostics {
         public static void CollapseRouter() {
             BilgeRouter.Router.Shutdown();
         }
+
+        public static void AddMessageHandler(IBilgeMessageHandler ibmh) {
+            BilgeRouter.Router.AddHandler(ibmh);
+        }
     }
 }
 
